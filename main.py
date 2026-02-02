b40cf0e259f62efab0bb34ceea6e110cc4afbddc
jRanu965 <jasnoorr965@deltalearns.ca>
2026-02-02 09:39:14 -0800

    ehe
    wyhw

---
+++ main.py
@@ -0,0 +1,7 @@
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Convert the Celsius temperature to Fahrenheit using the formula
+fahrenheit = (celsius * 9/5) + 32
+
+# Display the converted temperature
+print(f"{celsius}°C is equal to {fahrenheit}°F")
